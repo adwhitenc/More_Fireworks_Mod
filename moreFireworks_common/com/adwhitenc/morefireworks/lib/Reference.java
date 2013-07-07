927dad8d2c2c4ea4ca7bc276c17b0b625963c229
adwhitenc <adwhitenc@gmail.com>
2013-07-06 23:56:30 -0400

    Added the very core of the mod itself and information to be shown in the Mods tab in Minecraft.

---
+++ moreFireworks_common/com/adwhitenc/morefireworks/lib/Reference.java
@@ -0,0 +1,7 @@
+package com.adwhitenc.morefireworks.lib;
+
+public class Reference {
+	public static final String MOD_ID = "mod_MoreFireworks";
+	public static final String MOD_NAME = "More Fireworks";
+	public static final String VERSION = "0.0.1";
+}
